eb9a011e949632ba65aa544625073b0c54496326
SiddhantMondal <siddhantmondal9@gmail.com>
2024-04-03 00:07:01 +0530

    fruitsjavascripthighlighted

---
+++ fruitshighilted.js
@@ -0,0 +1,10 @@
+
+const fruits = document.getElementsByClassName("fruit");
+
+      fruits[2].style.backgroundColor = "yellow";
+
+      
+      for (let i = 0; i < fruits.length; i++) 
+      {
+        fruits[i].style.fontWeight = "bold";
+      }
