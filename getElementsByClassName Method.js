374e289e02ea0d13008893c8c7f4672bbb745eab
SiddhantMondal <siddhantmondal9@gmail.com>
2024-04-03 01:08:54 +0530

    Added my project

--- getElementsByClassName Method.js
+++
@@ -1,6 +0,0 @@
-const fruits = document.getElementsByClassName("fruit");
-
-for (let i = 0; i < fruits.length; i++) {
-    fruits[2].style.backgroundColor = "yellow"; 
-    fruits[i].style.fontWeight = "bold"; 
-}
---
+++ getElementsByTagName Method.js
@@ -0,0 +1,7 @@
+ const  listitems =  document.getElementsByTagName('li');
+ listitems[4]. style.color = 'blue';
+ 
+ for (let i = 0; i < listitems.length; i++) 
+ {
+    listitems[i].style.fontStyle = 'italic';
+}
